73e16dbc8b6d1b2e9d65f2cd6bd54a87955a8f38
panfeng <pf@.qq.com>
2022-10-21 11:24:05 +0800

    v1

--- readme.md
+++ readme.md
@@ -1,4 +1,28 @@
 2022-10-21 panfeng 哈尔滨工程大学
 1、带有时间戳的转换方法-moose2fullbag.py 
-2、无时间戳的转换方法Bin2ros_ws，目前需要用rosbag record /topics 记录//
+2、无时间戳的转换方法Bin2ros_ws//
 
+使用前先激活环境 conda activate bin2bag
+
+
+
+
+
+modify log:
+2022-10-21:
+1、原代码非顺序读取，改进为按bin文件编号顺序读取
+velo_filenames = sorted(os.listdir(file_path))  # get all of them
+2、原代码为发布点云格式，改进为保存为rosbag
+import rosbag
+compression = rosbag.Compression.NONE
+bag = rosbag.Bag(filtered_bag_path, 'w',    compression=compression)
+bag.write("/point_cloud",cloud,rospy.get_rostime())
+print(bag)
+bag.close()
+3、原代码为xyz格式，改进为xyzI增加强度信息
+fields = [PointField('x', 0, PointField.FLOAT32, 1),
+    PointField('y', 4, PointField.FLOAT32, 1),
+    PointField('z', 8, PointField.FLOAT32, 1),
+    PointField('intensity', 12, PointField.FLOAT32, 1)]
+cloud = pc2.create_cloud(point_cloud2.header, fields, point_data)
+保存git版本
--- src/rospy_rviz/script/rospy_rviz.py
+++ src/rospy_rviz/script/rospy_rviz.py
@@ -5,7 +5,7 @@ import os
 import numpy as np
 import rospy
 import rosbag
- 
+from sensor_msgs.msg import CameraInfo, Imu, PointField, NavSatFix
 from visualization_msgs.msg import *
 from sensor_msgs.msg import PointCloud2
 from sensor_msgs import point_cloud2 as pc2
@@ -38,20 +38,28 @@ def main():
     point_cloud2.header.frame_id = "/velodyne"
  
     file_path = rospy.get_param('file_path', "")  # 获取一个全局参数
+    filtered_bag_path = rospy.get_param('filtered_bag_path')
     file_name = get_data()
     compression = rosbag.Compression.NONE
 
-    bag = rosbag.Bag("filtered.bag", 'w',    compression=compression)
+    bag = rosbag.Bag(filtered_bag_path, 'w',    compression=compression)
     # while True:
     for file in file_name:
         point_data = np.fromfile((file_path + file), dtype=np.float32, count=-1).reshape([-1, 4])
         # point_data = point_data[:10]
-        cloud = pc2.create_cloud_xyz32(point_cloud2.header, point_data[:, :3])
-        bag.write("/point_cloud",cloud)
+        fields = [PointField('x', 0, PointField.FLOAT32, 1),
+            PointField('y', 4, PointField.FLOAT32, 1),
+            PointField('z', 8, PointField.FLOAT32, 1),
+            PointField('intensity', 12, PointField.FLOAT32, 1)]
+        cloud = pc2.create_cloud(point_cloud2.header, fields, point_data)
+        # cloud = pc2.create_cloud_xyz32(point_cloud2.header, point_data[:, :3])
+        bag.write("/point_cloud",cloud,rospy.get_rostime())
+
         pub_cloud.publish(cloud)
 
         # 控制发布频率
         rate.sleep()
+    print(bag)
     bag.close()
 
 if __name__ == "__main__":    
